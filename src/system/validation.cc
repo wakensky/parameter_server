2ae5c45bc8ac89e78d7275ead905a856b31e3878
D Wu <wakensky@gmail.com>
2015-02-01 01:32:05 +0800

    [ADD] Not complete Validation. Data Preparing is ready

---
+++ src/system/validation.cc
@@ -0,0 +1,257 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "util/split.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdexcept>
+#include <gperftools/malloc_extension.h>
+#include "base/shared_array_inl.h"
+#include "system/validation.h"
+
+namespace PS {
+
+Validation::Validation() {
+  ;
+}
+
+Validation::~Validation() {
+  ;
+}
+
+void Validation::init(
+  const string& identity,
+  LM::DataConfig& file_list,
+  PathPicker* path_picker) {
+  CHECK(!identity.empty());
+  identity_ = identity;
+  path_picker_ = path_picker;
+  file_list_ = file_list;
+}
+
+bool Validation::download(const Task& preprocess_task) {
+  // fill group_ranges_
+  for (int i = 0; i < preprocess_task.partition_info_size(); ++i) {
+    const GroupID grp_id = preprocess_task.partition_info(i).fea_grp();
+    Range<FullKey> global_range(preprocess_task.partition_info(i).key());
+    group_ranges_[grp_id].push_back(global_range.begin());
+    group_ranges_[grp_id].push_back(global_range.end());
+  }
+
+  // sort and unique
+  size_t number_of_blocks = 0;
+  for (auto& ranges : group_ranges_) {
+    std::sort(ranges.begin(), ranges.end());
+    auto last = std::unique(ranges.begin(), ranges.end());
+    ranges.erase(last, ranges.end());
+
+    CHECK_GE(ranges.size(), 2);
+    if (!ranges.empty()) {
+      number_of_blocks += ranges.size() - 1;
+    }
+  }
+  CHECK_EQ(number_of_blocks, preprocess_task.partition_info_size());
+
+  // read training files line by line
+  LineReader line_reader(file_list, FLAGS_validation_line_limit);
+  string line;
+  // I restrict the number of lines in each package,
+  //   making memory usage controllable
+  size_t package_line_count = 0;
+  ExampleID example_id = 0;
+  ExampleText example_text;
+  while (!(example_text.text = line_reader.readLine()).empty()) {
+    example_text.id = example_id++;
+    pending_examples_.push(example_text);
+    package_line_count++;
+
+    if (package_line_count >= FLAGS_validation_package_volumn) {
+      // wait
+      LI << "Validation::download is waiting package [" << package_id << "]";
+      while (1) {
+        if (pending_lines_.size() <= 0 - FLAGS_validation_thread_num) {
+          break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }
+      LI << "Validation::download finishes waiting package [" << package_id << "]";
+    }
+
+    // dump package
+    CHECK(dumpPackages());
+    package_line_count = 0;
+  }
+  CHECK(dumpPackages());
+  package_line_count = 0;
+}
+
+void Validation::exampleThreadFunc() {
+  ExampleParser parser;
+  parser.init(file_list_.text(), file_list_.ignore_feature_group());
+
+  auto group_ranges = group_ranges_;
+
+  // Separate one example to different batches
+  LivePackageHashMap separated_one_example;
+
+  auto separate = [](
+    const ExampleID example_id;
+    const Example& full_proto,
+    const std::unordered_map<GroupID, std::vector<FullKey>>& group_ranges,
+    LivePackageHashMap& separated_one_example) {
+    separated_one_example.clear();
+
+    ValidationPartialExample partial_proto;
+    std::vector<FullKey> partial_keys;
+
+    for (int i = 0; i < full_proto.slot_size(); ++i) {
+      const GroupID group_id = full_proto.slot(i).id();
+      auto& range_vec = group_ranges[group_id];
+
+      if (!range_vec.empty()) {
+        size_t last_range_idx = 0;
+        for (int j = 0; j < full_proto.slot(i).key_size(); ++j) {
+          FullKey key = full_proto.slot(i).key(j);
+
+          if (range_vec[last_range_idx] <= key) {
+            // store
+            if (!partial_keys.empty()) {
+              Range<FullKey> global_range(range_vec[last_range_idx - 1], range_vec[last_range_idx]);
+
+              separated_one_example[BatchID(group_id, global_range)].proto = partial_proto;
+              separated_one_example[BatchID(group_id, global_range)].keys = partial_keys;
+            }
+
+            // clear
+            partial_proto.Clear();
+            partial_keys.clear();
+
+            // set example id
+            partial_proto.set_id(example_id);
+          }
+
+          // locate
+          while (range_vec[last_range_idx] <= key &&
+                 last_range_idx < range_vec.size()) {
+            ++last_range_idx;
+          }
+
+          if (last_range_idx > 0 && last_range_idx < range_vec.size()) {
+            partial_proto.add_keys(key);
+            partial_keys.push_back(key);
+          } else {
+            // TODO
+            LL << "cannot locate key under group x";
+            break;
+          }
+        }
+      }
+    }
+  };
+
+  while (go_on_) {
+    // take out an example (plain text)
+    ExampleText example_text;
+    pending_examples_.pop(example_text);
+
+    // remove tailing line-break
+    if (!example_text.text.empty() && '\n' == example_text.text.back()) {
+      example_text.text.resize(example_text.text.size() - 1);
+    }
+
+    // parse to Example proto, which contains all features for an example
+    Example full_proto;
+    if (!parser.toProto(example_text.text, &full_proto)) {
+      LL << "Validation::exampleThreadFunc parses to full proto failed. [" <<
+        example_text.text << "]";
+      continue;
+    }
+
+    // separate keys into sub proto buffers
+    LivePackageHashMap separated_one_example;
+    separate(full_proto, group_ranges, separated_one_example);
+
+    // merge fragments to global live_packages_
+    for (const auto fragment : separated_one_example) {
+      LiveBatchHashMap::accessor accessor;
+      if (!live_packages_.find(accessor, fragment.first)) {
+        LL << ;
+        continue;
+      }
+
+      // merge partial proto
+      accessor->second.proto.add_partitial_examples(fragment.second.proto);
+
+      // merge partial keys
+      accessor->second.keys.insert(
+        accessor->second.keys.end(),
+        fragment.second.keys.begin(),
+        fragment.second.keys.end());
+    }
+  };
+}
+
+bool Validation::dumpPackages() {
+  // traverse all live packages and dump to disk
+  for (const auto& gid_ranges : group_ranges_) {
+    const GroupID group_id = gid_ranges.first;
+    if (gid_ranges.second.size() < 2) {
+      // TODO
+      LL << "ranges for group x not illegal";
+    }
+    for (size_t i = 0; i < gid_ranges.second.size() - 1; ++i) {
+      // produce batch_id
+      Range<FullKey> global_range(gid_ranges.second[i], gid_ranges.second[i + 1]);
+      BatchID batch_id(group_id, global_range);
+
+      LiveBatchHashMap::accessor live_accessor;
+      if (live_packages_.find(live_accessor, batch_id)) {
+        // proto file name
+        std::stringstream ss_proto;
+        ss_proto << "validation.proto." << identity_ << "." << group_id <<
+          "." << global_range.begin() << "-" << global_range.end();
+        const string proto_path = path_picker_->getPath(ss_proto.str());
+
+        // dump proto file
+        std::ofstream out(proto_path, ios::out | ios::trunc | ios::binary);
+        CHECK(out.good());
+        CHECK(live_accessor->second.proto.SerializeToOstream(&out));
+        out.close();
+
+        // release proto
+        live_accessor->second.proto.Clear();
+
+        // sort and unique
+        std::sort(live_accessor->second.keys.begin(), live_accessor->second.keys.end());
+        auto last = std::unique(merged.begin(), merged.end());
+        merged.erase(last, merged.end());
+
+        // dump unique keys
+        std::stringstream ss_uniq;
+        ss_uniq << "validation.uniq_key." << identity_ << "." << group_id <<
+          "." << global_range.begin() << "-" << global_range.end();
+        SArray<FullKey> uniq_keys({live_accessor->second.keys});
+        CHECK(uniq_keys.writeToFile(ss_uniq.str()));
+
+        // release keys
+        live_accessor->second.keys.clear();
+
+        // insert path into dumped_batches_
+        {
+          DumpedBatchHashMap::accessor dumped_accessor;
+          if (dumped_batches_.find(dumped_accessor, batch_id)) {
+            DumpedPackage new_package;
+            new_package.proto_path = proto_path;
+            new_package.uniq_keys_path = ss_uniq.str();
+
+            dumped_accessor->second.push_back(new_package);
+          } else {
+            LL << "critical lost";
+          }
+        }
+      }
+    }
+  }
+}
+
+};  // namespace PS
---
+++ src/system/validation.h
@@ -0,0 +1,175 @@
+#pragma once
+#include <cstdint>
+#include <cinttypes>
+#include <atomic>
+#include <array>
+#include <tbb/concurrent_queue.h>
+#include <tbb/concurrent_hash_map.h>
+#include <tbb/concurrent_unordered_set.h>
+#include "proto/task.pb.h"
+#include "util/common.h"
+#include "util/threadpool.h"
+#include "base/sparse_matrix.h"
+#include "system/path_picker.h"
+#include "system/ocean.h"
+
+namespace PS {
+
+DECLARE_bool(verbose);
+
+/*
+ * download validation data
+ * AUC calculation
+ */
+class Validation {
+  public:
+    using FullKey = uint64;
+    using Value = double;
+    using GroupID = int;
+    using TaskID = int;
+    using BatchID = Ocean::UnitID;
+    using PackageID = uint32;
+    using ExampleID = uint32;
+
+  public:
+    SINGLETON(Validation);
+    ~Validation();
+    Validation(const Validation& other) = delete;
+    Validation& operator= (const Validation& rhs) = delete;
+
+    // initialization
+    void init(
+      const string& identity,
+      const LM::DataConfig& file_list,
+      PathPicker* path_picker);
+
+    // download validation data to disk
+    bool download(const Task& preprocess_task);
+
+  private:
+    class LineReader {
+      public:
+        LineReader(const DataConfig& files, const size_t line_limit):
+          files_(files),
+          file_being_read_(nullptr),
+          readed_line_count_(0),
+          line_limit_(line_limit),
+          buf_(new char[kBufLen + 1]) {
+        }
+        LineReader(const LineReader& other) = delete;
+        LineReader& operator= (const LineReader& rhs) = delete;
+        // read files line by line
+        // return: empty string means all files exhausted
+        string readLine() {
+          if (0 != readed_line_count_ && readed_line_count_ >= line_limit_) {
+            if (file_being_read_ != nullptr) {
+              file_being_read_->close();
+              file_being_read_ = nullptr;
+            }
+            return string();
+          }
+
+          while (1) {
+            // open the next file
+            if (nullptr == file_being_read_) {
+              if (file_idx_ >= files_.file_size()) {
+                return string();
+              }
+
+              while (file_idx_ < files_.file_size()) {
+                file_being_read_ = File::open(ithFile(files_, file_idx_++));
+                if (nullptr != file_being_read_) {
+                  break;
+                } else {
+                  LL << "Validation::LineReader open failed [" <<
+                    files_.file(file_idx_ - 1) << "]";
+                }
+              }
+              if (nullptr == file_being_read_) {
+                // all files exhausted
+                return string();
+              }
+            }
+
+            // read one line
+            const char* ret = file_being_read_->readLine(buf_.get(), kBufLen);
+            if (nullptr == ret) {
+              // current file exhausted
+              file_being_read_->close();
+              file_being_read_ = nullptr;
+            } else {
+              readed_line_count_++;
+              return string(buf_.get());
+            }
+          }
+        }
+      private:
+        const size_t kBufLen = 1024 * 1024;
+        const DataConfig files_;
+        // which file is being read
+        size_t file_idx_;
+        File* file_being_read_;
+        // how many lines have been read
+        size_t readed_line_count_;
+        // maximum lines I could read
+        // 0 means no limit
+        const size_t line_limit_;
+        // line buffer
+        std::unique_ptr<char[]> buf_;
+    };
+
+    struct ExampleText {
+      ExampleID id;
+      string text;
+    };
+
+    struct DumpedPackage {
+      string proto_path;
+      string uniq_keys_path;
+    };
+
+    struct LivePackage {
+      ValidationData proto;
+      std::vector<FullKey> keys;
+    };
+
+  private:
+    Validation();
+
+    // dump live_packages_ onto disk
+    // dump packages associated with the same examples to the disk
+    // We separated FLAGS_VALIDATION_BATCH_VOLUMN examples into several packages
+    // Each package contains training features within a specified BatchID ({group, key_range})
+    bool dumpPackages();
+
+    // sub-threads process examples
+    void exampleThreadFunc();
+
+  private:
+    string identity_;
+    PathPicker* path_picker_;
+    // which validation files I should download
+    DataConfig file_list_;
+    // grp_id -> (N+1) Fullkeys
+    //   N: the number of column partitioned blocks
+    //   the last key is the ending guard
+    std::unordered_map<GroupID, std::vector<FullKey>> group_ranges_;
+    // examples need to be processed
+    tbb::concurrent_bounded_queue<std::<ExampleID, string>> pending_examples_;
+
+    // BatchID -> dumped proto path
+    using DumpedBatchHashMap = tbb::concurrent_hash_map<BatchID, std::vector<DumpedPackage>>;
+    DumpedBatchHashMap dumped_batches_;
+
+    // BatchID -> one package
+    using LivePackageHashMap = tbb::concurrent_hash_map::<BatchID, LivePackage>;
+    LivePackageHashMap live_packages_;
+
+    // sub threads who process the original examples (in text format)
+    std::vector<std::thread> example_threads_;
+
+    // ExampleID -> {actual label, model's prediction}
+    tbb::concurrent_hash_map<ExampleID, std::pair<double, double>> label_prediction_;
+};
+
+}; // namespace PS
